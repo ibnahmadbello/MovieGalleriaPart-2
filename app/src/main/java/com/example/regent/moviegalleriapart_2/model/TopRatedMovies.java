cf8e48d9be688126792f876783d50bb89eac827c
ibnahmad <justforlearningcode@gmail.com>
2018-09-14 22:24:27 +0100

    JSON response converted to POJO using GSON

---
+++ app/src/main/java/com/example/regent/moviegalleriapart_2/model/TopRatedMovies.java
@@ -0,0 +1,77 @@
+
+package com.example.regent.moviegalleriapart_2.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class TopRatedMovies {
+
+    @SerializedName("page")
+    @Expose
+    private int page;
+    @SerializedName("total_results")
+    @Expose
+    private int totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private int totalPages;
+    @SerializedName("results")
+    @Expose
+    private List<Result> results = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public TopRatedMovies() {
+    }
+
+    /**
+     * 
+     * @param results
+     * @param totalResults
+     * @param page
+     * @param totalPages
+     */
+    public TopRatedMovies(int page, int totalResults, int totalPages, List<Result> results) {
+        super();
+        this.page = page;
+        this.totalResults = totalResults;
+        this.totalPages = totalPages;
+        this.results = results;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+}
