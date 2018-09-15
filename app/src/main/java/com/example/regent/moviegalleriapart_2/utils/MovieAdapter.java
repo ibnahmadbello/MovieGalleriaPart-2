684e05dcc549ff9d55be80bd7b38924934354c4c
ibnahmad <justforlearningcode@gmail.com>
2018-09-15 18:00:34 +0100

    Row item list

---
+++ app/src/main/java/com/example/regent/moviegalleriapart_2/utils/MovieAdapter.java
@@ -0,0 +1,23 @@
+package com.example.regent.moviegalleriapart_2.utils;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
