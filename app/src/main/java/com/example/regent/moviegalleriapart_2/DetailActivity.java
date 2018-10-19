b3a71f473f53bed0051b4a3189f340d6f1a94949
ibnahmad <justforlearningcode@gmail.com>
2018-10-19 11:10:26 +0100

    Added image to DetailActivity

--- app/src/main/java/com/example/regent/moviegalleriapart_2/DetailActivity.java
+++ app/src/main/java/com/example/regent/moviegalleriapart_2/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.regent.moviegalleriapart_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -10,7 +11,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.regent.moviegalleriapart_2.model.Result;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = DetailActivity.class.getSimpleName();
     public static final String EXTRA_POSITION = "extra_movie";
@@ -19,7 +20,7 @@ public class DetailActivity extends AppCompatActivity {
 
     private RatingBar movieRating;
     private ImageView movieImage;
-    private TextView movieReleaseDate, movieTitle, movieOverview;
+    private TextView movieReleaseDate, movieTitle, movieOverview, movieFavourite;
     Result result;
     private double numberRating;
 
@@ -33,6 +34,10 @@ public class DetailActivity extends AppCompatActivity {
         movieReleaseDate = findViewById(R.id.release_date_text_view);
         movieTitle = findViewById(R.id.movie_name_text_view);
         movieOverview = findViewById(R.id.movie_overview_text_view);
+        movieFavourite = findViewById(R.id.movie_favourite_text_view);
+
+
+        movieFavourite.setOnClickListener(this);
 
         result = (Result) getIntent().getSerializableExtra(MovieActivity.EXTRA);
 
@@ -55,8 +60,13 @@ public class DetailActivity extends AppCompatActivity {
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .centerCrop()
                 .crossFade()
-                .override(250, 250)
+                .override(500, 500)
                 .into(movieImage);
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
