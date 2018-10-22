6af4936e738488ed2d210ba70a5e6f73002d8d9a
Ibn Ahmad <justforlearningcode@gmail.com>
2018-10-22 13:25:11 +0100

    Gotten url to ReviewPAge

--- app/src/main/java/com/example/regent/moviegalleriapart_2/ReviewActivity.java
+++ app/src/main/java/com/example/regent/moviegalleriapart_2/ReviewActivity.java
@@ -1,13 +1,79 @@
 package com.example.regent.moviegalleriapart_2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    private static final String TAG = ReviewActivity.class.getSimpleName();
+
+    WebView mWebView;
+    ProgressBar mProgressBar;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        mWebView = findViewById(R.id.review_page_web_view);
+        mProgressBar = findViewById(R.id.review_page_progress_bar);
+
+        setupWebView();
+
     }
+
+    private void setupWebView(){
+        Intent receivedIntent = getIntent();
+        if (receivedIntent.hasExtra(Intent.EXTRA_TEXT)){
+            url = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
+        }
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setInitialScale(1);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setScrollbarFadingEnabled(false);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDisplayZoomControls(true);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100){
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+//            @Override
+//            public void onReceivedTitle(WebView view, String title) {
+//                AppCompatActivity appCompatActivity = getApplicationContext();
+//                appCompatActivity.getSupportActionBar().setSubtitle(title);
+//            }
+        });
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+        mWebView.loadUrl(url);
+    }
+
 }
